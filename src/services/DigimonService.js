27204e65b0edb2e7fd0775af50256dcb5a8763b7
CarlosLP18 <carloslopez1122@hotmail.com>
2024-12-30 10:13:23 -0300

    Modificacion en Main.js, DigimonService. Creacion del folder src/plugins/

--- src/services/DigimonService.js
+++ src/services/DigimonService.js
@@ -1,5 +1,15 @@
 import axiosInstances from './axios';
 
+export const DigimonList = (page) => {
+  return axiosInstances.DigiApi.get(`digimon?pageSize=9&page=${page}`)
+    .then(response => {
+      return response.data;
+    })
+    .catch(error => {
+      throw new Error('Error al buscar el Digimon: ' + error);
+    });
+}
+
 export const searchDigimon = (digimonName) => {
   return axiosInstances.DigiApi.get(`digimon/${digimonName}`)
     .then(response => {
@@ -12,6 +22,86 @@ export const searchDigimon = (digimonName) => {
 
 export const searchIdDigimon = (digimonId) => {
   return axiosInstances.DigiApi.get(`digimon/${digimonId}`)
+    .then(response => {
+      return response.data;
+    })
+    .catch(error => {
+      throw new Error('Error al buscar el Digimon: ' + error);
+    });
+}
+
+export const AttributeList = (page) => {
+  return axiosInstances.DigiApi.get(`attribute?page=${page}`)
+    .then(response => {
+      return response.data;
+    })
+    .catch(error => {
+      throw new Error('Error al buscar el Digimon: ' + error);
+    });
+}
+
+export const searchAttribute = (attributeSearch) => {
+  return axiosInstances.DigiApi.get(`attribute/${attributeSearch}`)
+    .then(response => {
+      return response.data;
+    })
+    .catch(error => {
+      throw new Error('Error al buscar el Digimon: ' + error);
+    });
+}
+
+export const FieldList = (page) => {
+  return axiosInstances.DigiApi.get(`field?page=${page}`)
+    .then(response => {
+      return response.data;
+    })
+    .catch(error => {
+      throw new Error('Error al buscar el Digimon: ' + error);
+    });
+}
+
+export const searchField = (fieldSearch) => {
+  return axiosInstances.DigiApi.get(`field/${fieldSearch}`)
+    .then(response => {
+      return response.data;
+    })
+    .catch(error => {
+      throw new Error('Error al buscar el Digimon: ' + error);
+    });
+}
+
+export const TypeList = (page) => {
+  return axiosInstances.DigiApi.get(`type?page=${page}`)
+    .then(response => {
+      return response.data;
+    })
+    .catch(error => {
+      throw new Error('Error al buscar el Digimon: ' + error);
+    });
+}
+
+export const searchType = (typeSearch) => {
+  return axiosInstances.DigiApi.get(`type/${typeSearch}`)
+    .then(response => {
+      return response.data;
+    })
+    .catch(error => {
+      throw new Error('Error al buscar el Digimon: ' + error);
+    });
+}
+
+export const SkillList = (page) => {
+  return axiosInstances.DigiApi.get(`skill?page=${page}`)
+    .then(response => {
+      return response.data;
+    })
+    .catch(error => {
+      throw new Error('Error al buscar el Digimon: ' + error);
+    });
+}
+
+export const searchSkill = (skillSearch) => {
+  return axiosInstances.DigiApi.get(`skill/${skillSearch}`)
     .then(response => {
       return response.data;
     })
